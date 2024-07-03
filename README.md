b8b7bf28a4a6885b1ef53272d314b615d85b21be
Yihsiu Chen <yihsiu.chen@icloud.com>
2024-07-03 20:48:06 +0800

    Add webpack dev server and README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# Vanilla UI
+
+Pure js UI component. Just need one js file.
+
+## Development
+
+```bash
+npm run dev
+```
+
+## Build
+
+```bash
+npm run build
+```
+
+## Usage
+
+* **Toask**
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require('path');
 
-// module.exports = {
-//   entry: './src/vanilla-ui.js',
-//   output: {
-//     filename: 'vanilla-ui.min.js',
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-//   mode: 'production',
-// };
-
 module.exports = [
     {
         entry: './src/vanilla-ui.js',
         output: {
-          filename: 'vanilla-ui.min.js',
-          path: path.resolve(__dirname, 'dist'),
+            filename: 'vanilla-ui.min.js',
+            path: path.resolve(__dirname, 'dist'),
         },
         mode: 'production',
     },
     {
         entry: './src/vanilla-ui.js',
         output: {
-          filename: 'vanilla-ui.min.js',
-          path: path.resolve(__dirname, 'demo'),
+            filename: 'vanilla-ui.min.js',
+            path: path.resolve(__dirname, 'demo'),
         },
-        mode: 'production',
+        mode: 'development',
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'demo'),  // Directory to serve
+            }, 
+            compress: true,  // Enable gzip compression for everything served
+            port: 9000,  // Port number to listen on
+            open: true,  // Automatically open the browser
+            hot: true,  // Enable Hot Module Replacement
+        },
     },
 ];
